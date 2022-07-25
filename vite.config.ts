6a275d599dbc3c7bfd9fec15dc179649eae87b28
Zhang XiaoTong <1039146267@qq.com>
2022-07-25 12:00:27 +0800

    feat: 生产环境mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -6,41 +6,44 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      dts: 'src/declare/auto-imports.d.ts',
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      // 指定组件位置，默认是src/components
-      dirs: ['src/components'],
-      // 搜索子目录
-      deep: true,
-      // 配置文件生成位置
-      dts: 'src/declare/components.d.ts',
-      resolvers: [ElementPlusResolver()],
-    }),
-    viteMockServe({
-      ignore: /^_/,
-      mockPath: 'mock',
-      localEnabled: true, //本地启用mock
-      prodEnabled: true, //生产环境启用mock
-      injectCode: `
+export default defineConfig(({ command, mode }) => {
+
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        dts: 'src/declare/auto-imports.d.ts',
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        // 指定组件位置，默认是src/components
+        dirs: ['src/components'],
+        // 搜索子目录
+        deep: true,
+        // 配置文件生成位置
+        dts: 'src/declare/components.d.ts',
+        resolvers: [ElementPlusResolver()],
+      }),
+      viteMockServe({
+        ignore: /^_/,
+        mockPath: 'mock',
+        localEnabled: true, //本地开发环境启用mock
+        prodEnabled: mode === 'test', //生产环境启用mock(build模式为test时才启用)
+        injectCode: `
           import { setupProdMockServer } from '/mock/_createProductionServer';
           setupProdMockServer();
         `,
-    }),
-  ],
-  envDir: './env',
-  server: {
-    host: true
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
+      }),
+    ],
+    envDir: './env',
+    server: {
+      host: true
+    },
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+  }
 })
