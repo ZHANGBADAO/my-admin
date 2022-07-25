410fac4e15bff2697894169b03dffc01a48ac1a8
Zhang XiaoTong <1039146267@qq.com>
2022-07-25 16:54:25 +0800

    feat: 角色管理页面

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig,loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -7,7 +7,6 @@ import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-
   return {
     plugins: [
       vue(),
@@ -27,9 +26,9 @@ export default defineConfig(({ command, mode }) => {
       }),
       viteMockServe({
         ignore: /^_/,
-        mockPath: "mock",
+        mockPath: 'mock',
         localEnabled: true, //本地开发环境启用mock
-        prodEnabled: mode === "test", //生产环境启用mock(build模式为test时才启用)
+        prodEnabled: mode === 'test', //生产环境启用mock(build模式为test时才启用)
         injectCode: `
           import { setupProdMockServer } from '/mock/_createProductionServer';
           setupProdMockServer();
@@ -38,12 +37,12 @@ export default defineConfig(({ command, mode }) => {
     ],
     envDir: './env',
     server: {
-      host: true
+      host: true,
     },
     resolve: {
       alias: {
-        '@': '/src'
-      }
+        '@': '/src',
+      },
     },
   }
 })
