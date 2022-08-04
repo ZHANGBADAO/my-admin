66ec13f1f5c2ea365f17632a61e7861b57bb48b1
Zhang XiaoTong <1039146267@qq.com>
2022-08-04 14:43:04 +0800

    build: 升级vite版本

--- mock/_createProductionServer.ts
+++ mock/_createProductionServer.ts
@@ -1,12 +1,13 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 
-const modules = import.meta.globEager('./**/*.ts')
+const modules = import.meta.glob('./**/*.ts', { eager: true })
 
 const mockModules: any[] = []
 Object.keys(modules).forEach((key) => {
   if (key.includes('/_')) {
     return
   }
+  // @ts-ignore
   mockModules.push(...modules[key].default)
 })
 
