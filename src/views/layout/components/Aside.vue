67879401eb20f17706dff29dda59897782a4a6b9
Zhang XiaoTong <1039146267@qq.com>
2022-12-12 14:30:11 +0800

    feat: 顶部标签栏左右按钮滚动,批量关闭标签页

--- src/views/layout/components/Aside.vue
+++ src/views/layout/components/Aside.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-aside width="250px">
+  <el-aside :style="{ width: isCollapse ? 'unset' : '250px' }">
     <el-scrollbar style="padding-left: 20px">
       <el-menu
         style="border-right: 0; border-radius: 10px; overflow: hidden"
@@ -17,13 +17,16 @@ import AsideMenuItem from './AsideMenuItem.vue'
 import { useRoute } from 'vue-router'
 
 const useRouteStore = routerStore()
+
+//菜单数据
 const menuArr = computed(() => {
   return useRouteStore.menus
 })
 
 const $route = useRoute()
+//高亮打开的页面对应的菜单
 const defaultActive = ref($route.name)
-
+//监听激活的tab,高亮对应的菜单
 watch(
   () => useRouteStore.tabActivated,
   (val) => {
