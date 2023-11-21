11cead236b2d2870dae7a25c2973a15b28439d53
Zhang XiaoTong <1039146267@qq.com>
2023-11-21 16:42:57 +0800

    feat: tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { setupRouter } from './router'
 import { setupStore } from './store'
-import 'tailwindcss/tailwind.css'
+import '@/styles/tailwindcss.scss'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
 async function bootstrap() {
